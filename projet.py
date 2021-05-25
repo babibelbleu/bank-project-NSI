ef180f1d5ce7a27c896c044c021a8c73fe17f1a3
Quentin_Kimppienne <quentinkimppienne@gmail.com>
2021-05-25 21:57:28 +0200

    Voila

--- projet.py
+++ projet.py
@@ -14,21 +14,6 @@ def effacer():
         widget.destroy()
 
 
-# fonction pour definir la colonne pour choisir le mois de sa date de naissance
-def selected_mois():
-    Label(frame, text=combobox_mois.get()).pack()
-
-
-# fonction pour definir la colonne pour choisir l'ann�e de sa date de naissance
-def selected_annee():
-    Label(frame, text=combobox_annee.get()).pack()
-
-
-# fonction pour definir la colonne pour choisir le jour de sa date de naissance
-def selected_jour():
-    Label(frame, text=combobox_annee.get()).pack()
-
-
 # -------------------------------------------------------------------------------------------------------------------------
 # Partie du code pour le compte en francais
 def delete():
@@ -47,7 +32,7 @@ def page_compte():
     # création images
     height2 = 100
     width2 = 100
-    image2 = PhotoImage(file='data-science2.png').zoom(5).subsample(32)
+    image2 = PhotoImage(file='data-science.png').zoom(5).subsample(32)
     canvas2 = Canvas(frame, width=width2, height=height2, bg='#41B77F', bd=0, highlightthickness=0)
     canvas2.create_image(width / 2, height / 2, image=image2)
     canvas2.pack()
@@ -88,8 +73,12 @@ def page_compte():
                           bd=500)
     button_deco1.pack(pady=5)
 
+
 # fonction pour créer la page ou l'on peut créer son compte
 def creation_compte():
+    global combobox_annee
+    global combobox_mois
+    global combobox_jour
     # création de la frame principale
     frame_crea = Frame(frame, bg='#41B77F')
     frame_crea2 = Frame(frame, bg='#41B77F')
@@ -119,17 +108,17 @@ def creation_compte():
     # choix du sexe
     i = IntVar()
     check_homme = Radiobutton(frame_crea, text='Femme', value='1',
-                              bg='#41B77F', variablae = i)
+                              bg='#41B77F', variable=i)
     check_homme.pack()
     check_femme = Radiobutton(frame_crea, text='Homme', value='0',
-                              bg='#41B77F', variable = i)
+                              bg='#41B77F', variable=i)
     check_femme.pack()
 
     if i.get() == 1:
         sexe = 'F'
     else:
         sexe = 'M'
-        
+
     # nom/prénom/mot de passe
     var_nom = StringVar()
     label_nomcrea = Label(frame_crea, text="Nom", font=("Comic sans MS", 15),
@@ -137,15 +126,16 @@ def creation_compte():
     label_nomcrea.pack()
 
     nom_entry_creation = Entry(frame_crea, font=("Comic sans MS", 15),
-                               textvariable = var_nom, bg='#41B77F', fg='black')
+                               textvariable=var_nom, bg='#41B77F', fg='black')
     nom_entry_creation.pack()
 
     var_prenom = StringVar()
-    label_prenomcrea = Label(frame_crea, text="prenom", font=("Comic sans MS", 15), 
-                             textvariable = var_prenom, bg='#41B77F')
+    label_prenomcrea = Label(frame_crea, text="prenom", font=("Comic sans MS", 15),
+                             bg='#41B77F')
     label_prenomcrea.pack()
-  
-    prenom_entry_creation = Entry(frame_crea, font=("Comic sans MS", 15), bg='#41B77F', fg='black')
+
+    prenom_entry_creation = Entry(frame_crea, font=("Comic sans MS", 15), bg='#41B77F', fg='black',
+                                  textvariable=var_prenom)
     prenom_entry_creation.pack()
 
     label_mot_de_passe = Label(frame_crea, text="mot de passe", font=("Comic sans MS", 15), bg='#41B77F')
@@ -153,7 +143,7 @@ def creation_compte():
 
     var_mot_de_passe2 = StringVar()
     mot_de_passe_entry_creation = Entry(frame_crea, font=("Comic sans MS", 15),
-                                        textevariable = var_mot_de_passe2, 
+                                        textvariable=var_mot_de_passe2,
                                         bg='#41B77F', fg='black', show='*')
     mot_de_passe_entry_creation.pack()
 
@@ -161,16 +151,7 @@ def creation_compte():
     label = Label(frame_crea, text="Date de naissance",
                   font=("Comic sans MS", 20), bg='#41B77F')
     label.pack()
-    #mise en places des variable pour la date de naissance
-    cliked_annee = IntVar()
-    cliked_annee.get(annee[0])
-  
-    cliked_mois = IntVar()
-    cliked_mois.get(mois[0])
-    
-    cliked_jour = IntVar()
-    cliked_jour.get(jour[0])
-      
+
     # date de naissance
     texte_annee = Label(frame_crea2, text="Année", bg='#41B77F', font=("Damion", 15))
     texte_annee.grid(row=0, column=0)
@@ -182,33 +163,47 @@ def creation_compte():
     texte_jour.grid(row=0, column=2)
 
     # crétion du truc pour demander la date de naissance
-    annee = ["1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", 
-             "1979", "1980", "1981", "1982","1983","1984", "1985", "1986", "1987", 
-             "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996",
-             "1997","19998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", 
-             "2006", "2007", "2008", "2009", "2010","2011","2012", "2013", "2014", 
-             "2015", "2016", "2017", "2018", "2019", "2020", "2021", ]
-    combobox_annee = ttk.Combobox(frame_crea2, value=annee)
-    combobox_annee.current(0)
-    combobox_annee.bind("<<CombobocSelected>>", selected_annee)
-    combobox_annee.grid(row=1, column=0)
+    annee_valeur = ["1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978",
+                    "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987",
+                    "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996",
+                    "1997", "19998", "1999", "2000", "2001", "2002", "2003", "2004", "2005",
+                    "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014",
+                    "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
 
-    mois = ["01", "02", "03", "04", "05", "06", "07",
-            "08", "09", "10", "11","12"]
+    clicked_annee = StringVar()
+    clicked_annee.set(annee_valeur[0])
 
-    combobox_mois = ttk.Combobox(frame_crea2, value=mois)
-    combobox_mois.current(0)
-    combobox_mois.bind("<<ComboboxSelected>>", selected_mois)
-    combobox_mois.grid(row=1, column=1)
+    annee = OptionMenu(frame_crea2,clicked_annee,*annee_valeur)
+    annee.grid(row=1, column=0)
 
-    jour = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
-            "14", "15", "16", "17", "18", "19","20", "21","22", "23", "24", "25",
-            "26", "27", "28", "29", "30", "31"]
+    mois_valeur = ["01", "02", "03", "04", "05", "06", "07",
+                   "08", "09", "10", "11", "12"]
 
-    combobox_jour = ttk.Combobox(frame_crea2, value=jour)
-    combobox_jour.current(0)
-    combobox_jour.bind("<<ComboboxSelected>>", selected_jour)
-    combobox_jour.grid(row=1, column=2)
+    clicked_mois = StringVar()
+    clicked_mois.set(mois_valeur[0])
+
+    mois = OptionMenu(frame_crea2, clicked_mois, *mois_valeur)
+    mois.grid(row=1, column=1)
+
+    jour_valeur = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+                   "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25",
+                   "26", "27", "28", "29", "30", "31"]
+
+    clicked_jour = StringVar()
+    clicked_jour.set(jour_valeur[0])
+
+    jour = OptionMenu(frame_crea2, clicked_jour, *jour_valeur)
+    jour.grid(row=1, column=2)
+
+
+    # mise en place des variable pour créer le compte
+
+    L = []
+    L.append(clicked_annee)
+    L.append(clicked_mois)
+    L.append(clicked_jour)
+
+    date = "-".join(L)
 
     # création du bouton de retour en arriéres
     button_retour = Button(bottomframe, text="retour",
@@ -221,7 +216,9 @@ def creation_compte():
     button_suite = Button(bottomframe, text="créer un compte",
                           font=("Comic sans MS", 15),
                           bg='#007FFF', fg='#40E0D0', activeforeground='#40E0D0',
-                          activebackground='#007FFF', bd='1', command=lambda: [dblink.insert_user_data(var_prenom,sexe),effacer(), page_connecte(), connecte()])
+                          activebackground='#007FFF', bd='1',
+                          command=lambda: [dblink.insert_user_data(var_nom, var_prenom, sexe, date, var_mot_de_passe2),
+                                           effacer(), connecte()])
     button_suite.pack(side=LEFT)
 
     frame_crea.pack(expand=YES)
@@ -290,7 +287,7 @@ def parametre():
 
 
 # fontion qui crée la frame de la premiere page du compte lors de la connexion
-def connecte(): 
+def connecte():
     global mon_menu
     mon_menu = Menu(windows)
     windows.config(menu=mon_menu, bg='#41B77F')
@@ -311,7 +308,7 @@ def connecte():
     label_slogan1 = Label(frame, text="La banque de tous les lycéens sauf les Littéraires",
                           font=("Damion", 20), bg='#41B77F')
     label_slogan1.pack()
- 
+
     width3 = 100
     height3 = 100
     image3 = PhotoImage(file='data-science.png').zoom(5).subsample(32)
@@ -330,7 +327,7 @@ def connecte():
     ball = can.create_oval(100, 100, 150, 150, fill='yellow')
     x = 1
     y = 2
-    
+
     while True:
         try:
             can.move(ball, x, y)
@@ -342,7 +339,6 @@ def connecte():
                 y = y * -1
         except:
             break
-
 
 
 # -------------------------------------------------------------------------------------------------------------------------
@@ -460,8 +456,6 @@ def menu_espagnol():
 # ------------------------------------------------------------------------------------------------------------------------
 # page principalee
 global windows
-global var_utilisateur
-global var_mot_de_passe
 windows = Tk()
 # personnalier la fenêtre
 windows.title("Bank Terminale scientifique")
@@ -483,7 +477,7 @@ label_slogan.pack()
 width = 100
 height = 100
 image = PhotoImage(file='data-science.png').zoom(5).subsample(32)
-canvas = Canvas(frame,width=width, height=height, bg='#41B77F', bd=0, highlightthickness=0)
+canvas = Canvas(frame, width=width, height=height, bg='#41B77F', bd=0, highlightthickness=0)
 canvas.create_image(width / 2, height / 2, image=image)
 canvas.pack()
 
@@ -494,12 +488,14 @@ label_title3.pack()
 
 # creation de la zone de texte pour le nom
 var_utilisateur = StringVar()
-nom_entry1 = Entry(frame, font=("Comic sans MS", 20),textvariable = var_utilisateur, bg='#41B77F', fg='black')
+nom_entry1 = Entry(frame, font=("Comic sans MS", 20), textvariable=var_utilisateur,
+                   bg='#41B77F', fg='black')
 nom_entry1.pack()
 
 # creation de la zone de texte pour le nom
+var_mot_de_passe = StringVar()
 label_title4 = Label(frame, text="mot de passe", font=("Comic sans MS", 20), bg='#41B77F',
-                     bd=0, highlightthickness=0)
+                     bd=0, highlightthickness=0, textvariable=var_mot_de_passe)
 label_title4.pack()
 
 # creation de la zone de texte pour le mot de passe
@@ -515,7 +511,7 @@ button.pack(pady=5)
 # création bouton création compte
 button1 = Button(frame, text="se créer un compte", font=("Comic sans MS", 20),
                  bg='#007FFF', fg='#40E0D0', activeforeground='#40E0D0', activebackground='#007FFF',
-                 bd='1', command=lambda: [effacer(), creation_compte()])
+                 command=lambda: [effacer(), creation_compte()])
 button1.pack(pady=5)
 
 # création bouton decoration
